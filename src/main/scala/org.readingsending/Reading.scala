59bb79f5f9afb548a8cefde5eb8624cd12de8f0e
Mahmoud <mahmoud.abusarah@outlook.com>
2023-10-11 10:27:23 -0400

    Initial commit

---
+++ src/main/scala/org.readingsending/Reading.scala
@@ -0,0 +1,45 @@
+package org.readingsending
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+
+object Reading extends App {
+  val spark = SparkSession.builder.appName("KafkaToJson").master("local[*]").getOrCreate()
+
+  // Define the Kafka parameters
+  val kafkaParams = Map(
+    "kafka.bootstrap.servers" -> "ip-172-31-13-101.eu-west-2.compute.internal:9092",
+    "key.deserializer" -> "org.apache.kafka.common.serialization.StringDeserializer",
+    "value.deserializer" -> "org.apache.kafka.common.serialization.StringDeserializer",
+    "group.id" -> "group1",
+    "auto.offset.reset" -> "earliest",
+    "enable.auto.commit" -> "false"
+  )
+
+  // Define the Kafka topic to subscribe to
+  val kafkaTopic = "FraudAPIaba"
+
+  // Define the schema for the JSON messages
+  val schema = StructType(Seq(
+    StructField("amount", StringType, nullable = true),
+    StructField("id", StringType, nullable = true),
+    StructField("isFlaggedFraud", StringType, nullable = true),
+    StructField("isFraud", StringType, nullable = true),
+    StructField("nameDest", StringType, nullable = true),
+    StructField("nameOrig", StringType, nullable = true),
+    StructField("newbalanceDest", StringType, nullable = true),
+    StructField("newbalanceOrig", StringType, nullable = true),
+    StructField("oldbalanceDest", StringType, nullable = true),
+    StructField("oldbalanceOrg", StringType, nullable = true),
+    StructField("step", StringType, nullable = true),
+    StructField("type", StringType, nullable = true)
+  ))
+
+  // Read the JSON messages from Kafka as a DataFrame
+  val df = spark.readStream.format("kafka").option("kafka.bootstrap.servers", "ip-172-31-13-101.eu-west-2.compute.internal:9092").option("subscribe", kafkaTopic).option("startingOffsets", "earliest").load().select(from_json(col("Value").cast("string"), schema).alias("data")).selectExpr("data.*")
+
+
+  // Write the DataFrame as CSV files to HDFS
+  df.writeStream.format("csv").option("checkpointLocation", "/tmp/jenkins/kafkab").option("path", "/tmp/jenkins/kafkab/FraudApib").start().awaitTermination()
+
+}
---
+++ src/main/scala/org.readingsending/Sending.scala
@@ -0,0 +1,27 @@
+package org.readingsending
+
+import org.apache.spark.sql.SparkSession
+import requests._
+
+object Sending extends App {
+  val spark = SparkSession.builder().appName("My Spark Application").master("local[*]").getOrCreate()
+  while (true) {
+    import spark.implicits._
+    val apiUrl = "http://3.9.191.104:5000/api/data"
+    val response = get(apiUrl, headers = headers)
+    val total = response.text()
+    val dfFromText = spark.read.json(Seq(total).toDS)
+
+    // select the columns you want to include in the message
+
+    val messageDF = dfFromText.select($"amount", $"id", $"isFlaggedFraud", $"isFraud", $"nameDest",
+      $"nameOrig", $"newbalanceDest", $"newbalanceOrig", $"oldbalanceDest", $"oldbalanceOrg", $"step", $"type")
+
+    val kafkaServer: String = "ip-172-31-13-101.eu-west-2.compute.internal:9092"
+    val topicSampleName: String = "FraudAPIaba"
+
+    messageDF.selectExpr("CAST(id AS STRING) AS key", "to_json(struct(*)) AS value").selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)").write.format("kafka").option("kafka.bootstrap.servers", kafkaServer).option("topic", topicSampleName).save()
+    println("message is loaded to kafka topic")
+    Thread.sleep(10000) // wait for 10 seconds before making the next call
+  }
+}
